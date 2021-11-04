a99a28d84bd6cc5078f70840ff264dfd8cb9e144
Noah Cothran <noahcoth@gmail.com>
2021-11-04 18:18:45 -0500

    trying to fix write issue

--- tap-to-reply/tapapp/write.php
+++ tap-to-reply/tapapp/write.php
@@ -10,10 +10,14 @@ $myObj->en = $en;
 $myObj->up = $up;
 $myObj->un = $un;
 
+var_dump($myObj);
 $myJSON = json_encode($myObj);
-$f=fopen('responses.json','w'); //TODO: change to responses.json
-fwrite($f,$myJSON);
-fclose($f);
-//echo "<a href='myfile.json'>go to file</a>";
-header("Location: tapapp2.php");
+var_dump($myJSON);
+// $f=fopen('responses.json','w'); //TODO: change to responses.json
+// fwrite($f,$myJSON);
+// fclose($f);
+$bytes = file_put_contents("responses.json", $myJSON); 
+echo "The number of bytes written are $bytes.";
+echo "<a href='responses.json'>go to file</a>";
+//header("Location: tapapp2.php");
 
