2b571340ab46eb22cdd41cda9a96dea8cac7f1d2
Noah Cothran <noahcoth@gmail.com>
2021-11-05 18:49:28 -0500

    trying to fix write issue pt5

--- tap-to-reply/tapapp/write.php
+++ tap-to-reply/tapapp/write.php
@@ -21,31 +21,12 @@ $myJSON = json_encode($myObj);
 // echo "<a href='responses.json'>go to file</a>";
 // usleep(250000); //sleep for 0.25 sec
 
-$filename = 'responses.json';
-// Let's make sure the file exists and is writable first.
-if (is_writable($filename)) {
 
-    // In our example we're opening $filename in append mode.
-    // The file pointer is at the bottom of the file hence
-    // that's where $somecontent will go when we fwrite() it.
-    if (!$handle = fopen($filename, 'a')) {
-         echo "Cannot open file ($filename)";
-         exit;
-    }
-    //maybe fopen ???
-    // Write  to our opened file.
-    if (fwrite($handle, $myJSON) === FALSE) {
-        echo "Cannot write to file ($filename)";
-        exit;
-    }
-
-    echo "Success, wrote to file ($filename)";
-
-    fclose($handle);
-
-} else {
-    echo "The file $filename is not writable";
-    exit;
+$f = fopen('responses.json', 'w');
+if (fwrite($f, $myJSON)) {
+    echo "success";
 }
-header("Location: tapapp2.php");
+fclose($f);
+
+// header("Location: tapapp2.php");
 
