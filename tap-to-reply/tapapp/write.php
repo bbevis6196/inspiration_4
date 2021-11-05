91dc58a07f53bc20a07bdbd2bf55a993026f3424
Noah Cothran <noahcoth@gmail.com>
2021-11-05 18:34:15 -0500

    trying to fix write issue pt4

--- tap-to-reply/tapapp/write.php
+++ tap-to-reply/tapapp/write.php
@@ -10,15 +10,42 @@ $myObj->en = $en;
 $myObj->up = $up;
 $myObj->un = $un;
 
-var_dump($myObj);
+//var_dump($myObj);
 $myJSON = json_encode($myObj);
-var_dump($myJSON);
-// $f=fopen('responses.json','w'); //TODO: change to responses.json
+//var_dump($myJSON);
+// $f=fopen('responses.json','w');
 // fwrite($f,$myJSON);
 // fclose($f);
-$bytes = file_put_contents("responses.json", $myJSON); 
+//$bytes = file_put_contents("responses.json", $myJSON); 
 // echo "The number of bytes written are $bytes.";
 // echo "<a href='responses.json'>go to file</a>";
-usleep(250000); //sleep for 0.25 sec
+// usleep(250000); //sleep for 0.25 sec
+
+$filename = 'responses.json';
+// Let's make sure the file exists and is writable first.
+if (is_writable($filename)) {
+
+    // In our example we're opening $filename in append mode.
+    // The file pointer is at the bottom of the file hence
+    // that's where $somecontent will go when we fwrite() it.
+    if (!$handle = fopen($filename, 'a')) {
+         echo "Cannot open file ($filename)";
+         exit;
+    }
+    //maybe fopen ???
+    // Write  to our opened file.
+    if (fwrite($handle, $myJSON) === FALSE) {
+        echo "Cannot write to file ($filename)";
+        exit;
+    }
+
+    echo "Success, wrote to file ($filename)";
+
+    fclose($handle);
+
+} else {
+    echo "The file $filename is not writable";
+    exit;
+}
 header("Location: tapapp2.php");
 
