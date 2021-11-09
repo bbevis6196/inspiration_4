0b52141a8b6f7a2f445b9d2370b26fc91d29e6e7
Noah Cothran <noahcoth@gmail.com>
2021-11-08 20:51:17 -0600

    add bootstrap5 to index

--- index.php
+++ index.php
@@ -3,15 +3,18 @@
 
 <head>
     <title>Main page</title>
-    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
     <h1>hello there!</h1>
     <p>stuff about product here</p>
-    <a href="tap-to-reply/tap-mainpage.php">link to tap-to-reply page</a><br>
-    <a href="qwxt/qwxt-mainpage.php">link to qwxt page</a>
+    <div class="d-grid gap-3">
+        <button  class="btn btn-primary" onclick="window.location.href='tap-to-reply/tap-mainpage.php';">Go to tap-to-reply page</button>
+        <button  class="btn btn-primary" onclick="window.location.href='qwxt/qwxt-mainpage.php';">Go to qwxt page</button>
+    </div>
 </body>
 
 </html>
