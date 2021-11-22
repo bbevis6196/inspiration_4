7a378d33f5376cb160ea0297c57ab0bea1d9068a
Noah Cothran <noahcoth@gmail.com>
2021-11-21 18:31:35 -0600

    update styling and content of index

--- index.php
+++ index.php
@@ -8,26 +8,27 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
-<body>
-    <div class="container-fluid p-5 bg-secondary bg-gradient text-Red text-center">
+<body class="bg-light bg-gradient">
+    <div class="container-fluid p-5 bg-primary bg-gradient text-Red text-center">
         <h1>Inspiration 4</h1>
         
     </div>
-    <div>
-        <h2>Hello There!! </h2>
+    <div class="container d-flex align-items-center justify-content-center">
+        <h2>Hello There!</h2>
     </div>
-    <div>
-    <p>Placeholder for information of both apps and team </p>
+    <div class="container d-flex align-items-center justify-content-center">
+        <p>Tap-to-reply and QWXT are about making replying and getting the right reply easier. Click on of the buttons below to go to the main page of either Tap-to-reply or QWXT.</p> <br>
+        <p></p>
     </div>
     <div class="d-grid gap-3">
-        <button  class="btn btn-primary" onclick="window.location.href='tap-to-reply/tap-mainpage.php';">Go to tap-to-reply page</button>
-        <button  class="btn btn-primary" onclick="window.location.href='qwxt/qwxt-mainpage.php';">Go to qwxt page</button>
+        <button  class="btn btn-primary" onclick="window.location.href='tap-to-reply/tap-mainpage.php';">Go to Tap-to-reply main page</button>
+        <button  class="btn btn-primary" onclick="window.location.href='qwxt/qwxt-mainpage.php';">Go to QWXT main page</button>
     </div>
-    <div> 
-        <h1> Team Information</h1>
+    <!-- <div> 
+        <h2> Team Information</h1>
         <p> Members: </p>
         <p> Product:</p>
-    </div>
+    </div> -->
     
 </body>
 
