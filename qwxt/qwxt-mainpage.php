bafc6b2e20c654931ecc4581b99ae8172c17e960
Bevis <bbevis4@una.edu>
2021-11-30 12:19:27 -0600

    updated main-page

--- qwxt/qwxt-mainpage.php
+++ qwxt/qwxt-mainpage.php
@@ -11,7 +11,7 @@
 <body>
 <div class="container-fluid p-5 bg-secondary bg-gradient text-white text-center">
   <h1>Welcome to the QWXT Main Page!</h1>
-  <p>Placeholder</p> 
+  <p>The goal of QWXT is to answer a question, with a text. </p> 
   <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-chat-dots-fill" viewBox="0 0 16 16" style="margin-right: 10%">
     <path d="M16 8c0 3.866-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.584.296-1.925.864-4.181 1.234-.2.032-.352-.176-.273-.362.354-.836.674-1.95.77-2.966C.744 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7zM5 8a1 1 0 1 0-2 0 1 1 0 0 0 2 0zm4 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
   </svg>
@@ -48,7 +48,7 @@
       <img src="frustration.jpg" style="max-width: 70% ; height: auto" />
       <p></p>
 
-      <p>The goal of QWXT is to answer a question, with a text. The main interface of the app will display a question at the top of the window. Below that, there will be responses the user inputs as click-able buttons. Clicking on one of the options will display the selected response at the bottom of the window, simulating sending the reply.
+      <p>The main interface of the app will display a question at the top of the window. Below that, there will be responses the user inputs as click-able buttons. Clicking on one of the options will display the selected response at the bottom of the window, simulating sending the reply.
       </p>
       
     </div>
